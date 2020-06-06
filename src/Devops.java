26fe5d01d0e83f181febb03d88c51e00b3123e7a
Shubham Narula <Shubhamnarula31@gmail.com>
2020-06-06 22:57:28 +0530

    First Commit

---
+++ src/Devops.java
@@ -0,0 +1,20 @@
+
+public class Devops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Fisrt devops program");
+		Devops d=new Devops();
+		d.addValue(100, 20);
+		
+	}
+
+	
+	public void addValue(int a,int b)
+	{
+		int c=a-b;
+		
+		System.out.println("value is "+c);
+	}
+}
