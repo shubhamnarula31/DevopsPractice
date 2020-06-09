dabd0401fedd8cdc48102a5f60323f49534126b5
Shubham Narula <Shubhamnarula31@gmail.com>
2020-06-10 00:54:21 +0530

    poll SCM11

---
+++ Devops.java
@@ -0,0 +1,23 @@
+
+public class Devops {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Fisrt devops program");
+		System.out.println("Fisrt devops program11111");
+		Devops d=new Devops();
+		d.addValue(100, 20);
+		//Demo d1=new Demo();
+		
+	}
+
+	
+	public void addValue(int a,int b)
+	{
+		int c=a-b;
+		
+		System.out.println("value is "+c);
+	}
+}
